4de3dd4e0a9c683c39775c57cf9b260c13486226
Moundirbechikh <moundirbechikh03@gmail.com>
2026-02-09 15:29:36 +0100

    Update MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -2,17 +2,23 @@ import React from "react";
 
 const MovieCard = ({ image, title, description }) => {
   return (
-    <div className="bg-white bg-opacity-10 backdrop-blur-md shadow-lg rounded-2xl overflow-hidden w-72 md:w-96  border m-2 border-black">
-      <img 
-        src={image} 
-        alt={title} 
-        className="w-full h-48 object-cover"
-      />
-      <div className="p-4 text-center">
-        <h3 className="text-3xl font-parisienne font-bold text-black mb-2">{title}</h3>
-        <p className="text-sm text-black font-cursive">{description}</p>
-      </div>
-    </div>
+<div className="bg-white bg-opacity-10 backdrop-blur-md shadow-lg rounded-2xl overflow-hidden 
+                w-full max-w-xs sm:max-w-sm md:max-w-md border m-2 border-black">
+  <img 
+    src={image} 
+    alt={title} 
+    className="w-full h-40 sm:h-48 object-cover"
+  />
+  <div className="p-3 sm:p-4 text-center">
+    <h3 className="text-xl sm:text-2xl md:text-3xl font-parisienne font-bold text-black mb-2">
+      {title}
+    </h3>
+    <p className="text-xs sm:text-sm md:text-base text-black font-cursive">
+      {description}
+    </p>
+  </div>
+</div>
+
   );
 };
 
