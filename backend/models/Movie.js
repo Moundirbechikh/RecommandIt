e4a58936c3018801b024389e7520ed92eff6fb5e
Moundirbechikh <moundirbechikh03@gmail.com>
2026-02-16 15:33:57 +0100

    Refactor Movie schema to include additional fields

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,18 +1,17 @@
+// models/Movie.js
 const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
-  movieId: {
-    type: Number, // identifiant interne (ex: 1889)
-    required: true,
-  },
-  userId: {
-    type: String, // id utilisateur (JWT ou Mongo)
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now, // trace de la date d’ajout
-  },
+  movieId: { type: Number, required: true }, // identifiant interne
+  title: { type: String, required: true },
+  genres: { type: [String], default: [] },
+  year: { type: String },
+  description: { type: String },
+  actors: { type: [String], default: [] },
+  backdrop: { type: String },
+  description_clean: { type: String },
+  userId: { type: String, required: true }, // qui a ajouté le film
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Movie", movieSchema);
